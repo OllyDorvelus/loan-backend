25b87b824f7b671b2562ba86979beddec573a07a
Olivier Dorvelus <ollydorvelus@gmail.com>
2022-02-18 23:11:00 -0500

    remove pen_name and add abstract base to customer

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app.accounts import models
 
 # Register your models here.
+
+
+class LoanAccountAdmin(admin.ModelAdmin):
+    fields = ['user', 'balance', 'status', 'due_date']
+
+
+admin.site.register(models.LoanAccount, LoanAccountAdmin)
--- app/accounts/models.py
+++ app/accounts/models.py
@@ -3,6 +3,15 @@ from app.users.models import AbstractModel
 from djmoney.models.fields import MoneyField
 
 
+LOAN_STATUS = (
+    ('ACTIVE', 'active'),
+    ('PAST_DUE', 'past_due'),
+    ('PENDING', 'pending'),
+    ('PAID', 'paid'),
+    ('CLOSED', 'closed'),
+)
+
+
 class AccountBase(AbstractModel):
     balance = MoneyField(max_digits=10, decimal_places=2, default_currency='ZA')
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='accounts')
@@ -13,3 +22,7 @@ class AccountBase(AbstractModel):
 
 class LoanAccount(AccountBase):
     due_date = models.DateField()
+    status = models.CharField(max_length=14, choices=LOAN_STATUS, default='PENDING')
+
+    def __str__(self):
+        return f'{self.user.email} - {self.balance} - {self.status}'
