faa9e0730b1e015b8124cfb68ef6dc5c9a2aafb0
Olivier Dorvelus <ollydorvelus@gmail.com>
2022-02-18 23:30:14 -0500

    set up api urls

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from django.urls import include
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
