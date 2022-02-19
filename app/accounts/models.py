fb5ecb6c977bfad6ce27c59e37d0e9d855142fc0
Olivier Dorvelus <ollydorvelus@gmail.com>
2022-02-19 02:44:38 -0500

    add ZAR currency

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from app.users.models import AbstractModel
 from djmoney.models.fields import MoneyField
-
+from moneyed import ZAR
 
 LOAN_STATUS = (
     ('ACTIVE', 'active'),
@@ -12,8 +12,25 @@ LOAN_STATUS = (
 )
 
 
+class AccountManager(models.Manager):
+
+    def create_loan_account(self, user, balance, due_date, status='pending', **extra_fields):
+        if not user:
+            raise ValueError("Account must have user.")
+
+        if not balance:
+            raise ValueError("Account must have balance.")
+
+        if not due_date:
+            raise ValueError("Account must have a due date.")
+
+        account = self.model(user=user, balance=balance, due_date=due_date, status=status, **extra_fields)
+        account.save(using=self._db)
+        return account
+
+
 class AccountBase(AbstractModel):
-    balance = MoneyField(max_digits=10, decimal_places=2, default_currency='ZA')
+    balance = MoneyField(max_digits=10, decimal_places=2, default_currency=ZAR)
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='accounts')
 
     class Meta:
@@ -23,6 +40,8 @@ class AccountBase(AbstractModel):
 class LoanAccount(AccountBase):
     due_date = models.DateField()
     status = models.CharField(max_length=14, choices=LOAN_STATUS, default='PENDING')
+
+    objects = AccountManager()
 
     def __str__(self):
         return f'{self.user.email} - {self.balance} - {self.status}'
