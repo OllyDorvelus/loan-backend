396dc795746deb0c7adbec7f6c65b68ae3d70219
Olivier Dorvelus <ollydorvelus@gmail.com>
2022-02-21 18:39:54 -0500

    modify utils to reflect number on user model

--- app/script_utils.py
+++ app/script_utils.py
@@ -6,9 +6,9 @@ from app.api.whats_app_client import WhatsAppClient
 def send_balance_due_soon_notification():
     two_days_from_now = date.today() + timedelta(days=2)
     accounts = LoanAccount.objects.filter(status='ACTIVE', balance__amount__gt=0,
-                                          due_date=two_days_from_now).select_related('user', 'user__customer')
+                                          due_date=two_days_from_now).select_related('user')
     for account in accounts:
-        WhatsAppClient.send_balance_due_soon_message(account.user.customer.whatsapp_number,
+        WhatsAppClient.send_balance_due_soon_message(account.user.whatsapp_number,
                                                      account.user.full_name, account.total,
                                                      account.due_date)
         print(f'Sent notification to Customer: {account.user.full_name}')
@@ -17,19 +17,19 @@ def send_balance_due_soon_notification():
 def send_balance_due_warning_today_notification():
     today = date.today()
     accounts = LoanAccount.objects.filter(status='ACTIVE', balance__amount__gt=0,
-                                          due_date=today).select_related('user', 'user__customer')
+                                          due_date=today).select_related('user')
     for account in accounts:
-        WhatsAppClient.send_balance_due_warning_message(account.customer.whatsapp_number, account.user.full_name,
+        WhatsAppClient.send_balance_due_warning_message(account.user.whatsapp_number, account.user.full_name,
                                                         account.total, account.due_date)
         print(f'Sent notification to Customer: {account.user.full_name}')
 
 
 def send_balance_past_due_notification():
     today = date.today()
-    accounts = LoanAccount.objects.filter(status='ACTIVE', balance__amount__gt=0).select_related('user',
-                                                                                                 'user__customer')
+    accounts = LoanAccount.objects.filter(status='ACTIVE', balance__amount__gt=0).select_related('user')
+
     for account in accounts:
-        WhatsAppClient.send_past_due_message(account.user.customer.whatsapp_number, account.user.full_name,
+        WhatsAppClient.send_past_due_message(account.user.whatsapp_number, account.user.full_name,
                                              account.total, account.due_date)
         if (today - account.due_date).days >= 2:
             account.status = 'PAST_DUE'
