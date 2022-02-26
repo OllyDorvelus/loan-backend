f255f055f87aef5e5ee9adc7261712f882dc4e82
Olivier Dorvelus <ollydorvelus@gmail.com>
2022-02-26 18:53:42 -0500

    transaction

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -27,12 +27,14 @@ class AccountManager(models.Manager):
 
 
 class TransactionManager(models.Manager):
-    def create_transaction(self, account, amount, **extra_fields):
+    def create_transaction(self, account, amount, transaction_type, **extra_fields):
         if not account:
             raise ValueError("Transaction must have account")
         if not amount:
             raise ValueError("Transaction must have amount")
-        transaction = self.model(account=account, amount=amount, **extra_fields)
+        if not transaction_type:
+            raise ValueError("Transaction must have transaction type")
+        transaction = self.model(account=account, amount=amount, transaction_type=transaction_type, **extra_fields)
         transaction.save(using=self._db)
         return transaction
 
@@ -106,8 +108,17 @@ class LoanAccount(AccountBase):
 
 
 class Transaction(AbstractModel):
+    ADDED = 'ADDED'
+    DEDUCTED = 'DEDUCTED'
+    TRANSACTION_TYPE = (
+        (ADDED, 'added'),
+        (DEDUCTED, 'deducted')
+    )
     amount = MoneyField(max_digits=10, decimal_places=2, default_currency=ZAR)
     account = models.ForeignKey('accounts.LoanAccount', on_delete=models.CASCADE, related_name='transactions')
+    transaction_type = models.CharField(choices=TRANSACTION_TYPE, max_length=10)
+
+    objects = TransactionManager()
 
     def __str__(self):
         return f'{self.account.user.full_name} - {self.amount} - {self.created}'
@@ -184,6 +195,12 @@ def send_balance_change_message(sender, instance, **kwargs):
         if old_amount != new_amount:
             WhatsAppClient.send_submitted_application_message(instance.user.whatsapp_number, instance.user.full_name,
                                                               old_amount, new_amount)
+            new_transaction = {'amount': old_amount - new_amount, 'account': instance}
+            if new_amount > old_amount:
+                new_transaction['transaction_type'] = Transaction.ADDED
+            else:
+                new_transaction['transaction_type'] = Transaction.DEDUCTED
+            Transaction.objects.create_transaction(**new_transaction)
 
 
 pre_save.connect(send_message_when_pending_to_active, sender=LoanAccount)
