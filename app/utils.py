16f43a1f1a28e23b83bc5f4c2c32f40a596e88a1
Olivier Dorvelus <ollydorvelus@gmail.com>
2022-03-05 23:15:06 -0500

    utils

--- app/utils.py
+++ app/utils.py
@@ -2,9 +2,5 @@ def format_date(date):
     return date.strftime("%d-%m-%y")
 
 
-def apply_interest(amount, interest_rate=0.25):
-    return (amount * interest_rate) + amount
-
-
 def format_money(amount):
     return f'R{amount:,.2f}'.replace(',', ' ').replace('.', ',')
