350ef3ac6dbfe8d11400d4284a5e3e1250ca30dc
Olivier Dorvelus <ollydorvelus@gmail.com>
2022-04-16 13:24:15 -0400

    modify permissions

--- app/api/permissions.py
+++ app/api/permissions.py
@@ -30,3 +30,15 @@ class AuthenticatedCantPost(permissions.BasePermission):
         ):
             return False
         return True
+
+
+class IsAdminOrObjectOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method == "POST":
+            return request.user.is_authenticated
+        return request.user.is_superuser or request.user == obj.user
+
+
+class IsAdminOrUserOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_superuser or request.user == obj
