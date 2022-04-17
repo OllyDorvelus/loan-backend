b3baeb0cab671a4a67ccc2113a6194136c29095c
Olivier Dorvelus <ollydorvelus@gmail.com>
2022-04-17 16:46:32 -0400

    get permissions classes for account views

--- app/api/views/accounts.py
+++ app/api/views/accounts.py
@@ -6,9 +6,10 @@ from app.api.serializers.accounts import (
 from app.accounts.models import LoanAccount
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
-from app.api.permissions import IsAdminOrObjectOwnerToRead
+from app.api.permissions import IsAdminOrUserOwnerReadOnly
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.permissions import IsAdminUser
 
 APPLY_NAME = "Apply Loan"
 
@@ -16,9 +17,12 @@ APPLY_NAME = "Apply Loan"
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = LoanAccount.objects.all()
     serializer_class = AccountSerializer
-    permission_classes = [
-        IsAdminOrObjectOwnerToRead,
-    ]
+    permission_classes = [IsAdminUser]
+
+    def get_permissions(self):
+        if self.action == "retrieve":
+            self.permission_classes = [IsAdminOrUserOwnerReadOnly]
+        return super(AccountViewSet, self).get_permissions()
 
     def get_serializer_class(self):
         serializer = self.serializer_class
@@ -37,7 +41,9 @@ class AccountViewSet(viewsets.ModelViewSet):
         user = request.user
         if user.accounts.count() > 0:
             return Response(
-                {"message": "You already have an account with us."},
+                {
+                    "message": "You already applied for a loan and have an account with us."
+                },
                 status=HTTP_400_BAD_REQUEST,
             )
         return super().create(request, *args, *kwargs)
